b60c099c1a95f819a18d00d0c4b74d935fb51303
Kilotas <Alendos87@gmail.com>
2024-01-01 14:03:18 +0600

    first-commit

--- users/models.py
+++ users/models.py
@@ -1,8 +1,13 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        _('email address'),
+        unique=True
+    )
+    email_verify = models.BooleanField(default=False)
 
-
-# Create your models here.
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
